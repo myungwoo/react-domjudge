03e78e971e115b218897e508f7e5fdf17268e5d3
Myungwoo Chun <mc.tamaki@gmail.com>
2017-10-21 16:53:09 +0900

    [api] LEFT JOIN query 안 쓰기

--- db/submission.js
+++ db/submission.js
@@ -3,10 +3,10 @@ const pool = require('./pool');
 exports.getListByTeamContest = (cid, teamid) => {
   return new Promise((resolve, reject) => {
     pool.query(`SELECT s.submitid, s.teamid, s.probid, s.langid, s.submittime, cp.shortname, p.name, j.result, j.seen, s.valid, j.verified
-        FROM submission          as s
-        LEFT JOIN contestproblem as cp USING (probid, cid)
-        LEFT JOIN problem        as p  USING (probid)
-        LEFT JOIN judging        as j     ON (s.submitid = j.submitid AND j.valid = 1)
+        FROM submission     as s
+        JOIN contestproblem as cp USING (probid, cid)
+        JOIN problem        as p  USING (probid)
+        JOIN judging        as j     ON (s.submitid = j.submitid AND j.valid = 1)
         WHERE s.cid = ? AND s.teamid = ?
         ORDER BY s.submittime DESC, s.submitid DESC`, [cid, teamid], (err, res) => {
         if (err) reject(err);
@@ -18,11 +18,11 @@ exports.getListByTeamContest = (cid, teamid) => {
 exports.getDetailByTeam = (submitid, teamid) => {
   return new Promise((resolve, reject) => {
     pool.query(`SELECT p.probid, cp.shortname, p.name AS probname, submittime, s.valid, l.name AS langname, result, output_compile, verified, judgingid, s.cid
-        FROM judging             as j
-        LEFT JOIN submission     as s  USING (submitid)
-        LEFT JOIN language       as l  USING (langid)
-        LEFT JOIN problem        as p     ON (p.probid = s.probid)
-        LEFT JOIN contestproblem as cp    ON (cp.probid = p.probid AND cp.cid = s.cid)
+        FROM judging        as j
+        JOIN submission     as s  USING (submitid)
+        JOIN language       as l  USING (langid)
+        JOIN problem        as p     ON (p.probid = s.probid)
+        JOIN contestproblem as cp    ON (cp.probid = p.probid AND cp.cid = s.cid)
         WHERE j.submitid = ? AND teamid = ? AND j.valid = 1`, [submitid, teamid], (err, res) => {
         if (err) reject(err);
         resolve(res.map(e => {
@@ -36,10 +36,10 @@ exports.getDetailByTeam = (submitid, teamid) => {
 exports.getSampleRun = submitid => {
   return new Promise((resolve, reject) => {
     pool.query(`SELECT r.runresult, r.runtime, r.output_run, r.output_diff, r.output_error, t.rank, t.description
-        FROM judging          as j
-        LEFT JOIN submission  as s USING (submitid)
-        LEFT JOIN testcase    as t    ON (t.probid = s.probid AND t.sample = 1)
-        LEFT JOIN judging_run as r    ON (r.testcaseid = t.testcaseid AND r.judgingid = j.judgingid)
+        FROM judging     as j
+        JOIN submission  as s USING (submitid)
+        JOIN testcase    as t    ON (t.probid = s.probid AND t.sample = 1)
+        JOIN judging_run as r    ON (r.testcaseid = t.testcaseid AND r.judgingid = j.judgingid)
         WHERE submitid = ? ORDER BY t.rank`, [submitid], (err, res) => {
         if (err) reject(err);
         resolve(res.map(e => {
