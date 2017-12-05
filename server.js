efa4130ddf96884b2fc9b74ec6e1a644d00b9716
Myungwoo Chun <mc.tamaki@gmail.com>
2017-12-05 22:45:00 +0900

    Catch unhandledPromiseRejection

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ app.use('/api', require('./api'));
 if (process.env.NODE_ENV === 'production')
   app.use('/', express.static('client/build'));
 
+process.on('unhandledRejection', error => {
+  console.error('unhandledRejection', error.message); // eslint-disable-line no-console
+});
+
 app.listen(app.get('port'), () => {
   console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
 });
